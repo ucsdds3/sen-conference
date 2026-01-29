70b6bd46dc030551a21b90ef3d20faeb2e824817
Junnian Liu <liuj4467@Junnians-MacBook-Air.local>
2026-01-29 15:11:43 -0800

    skeleton for header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Image from 'next/image'
+import Logo from '@/app/assets/sen_logo.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <section>
+        <div className='flex w-full h-[160px] bg-[#0D1626]'>
+            <Image src={Logo} width={400} height={160} alt='SEN Logo' className='ml-[5%]'></Image>
+            <div className='flex w-[858px] h-[160px] justify-between items-center text-white mr-[50px]'>
+                <div>Home</div>
+                <div>Schedule</div>
+                <div>Speakers</div>
+                <div>Partners</div>
+                <div>FAQ</div>
+                <div>Register</div>
+            </div>
+        </div>
+    </section>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 export default function Home() {
   return (
-    <div className="text-red-500">Tailwind works</div>
+    <>
+      <section>
+        <Header />
+      </section>
+      <main>
+        <div>Hi there</div>
+      </main>
+      <section>
+        <Footer />
+      </section>
+    </>
   );
 }
