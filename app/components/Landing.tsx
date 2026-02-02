d556237ba20cfc358fd8d6e924110b4c7b92e128
nicolastoon <nicolastoon05@gmail.com>
2026-02-01 16:51:44 -0800

    header and footer on shared layout

---
+++ app/components/Landing.tsx
@@ -0,0 +1,10 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+export default function Home() {
+  return (
+      <main>
+        <div>Hi there</div>
+      </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,7 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Landing from "./components/Landing";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <section>
-        <Header />
-      </section>
-      <main>
-        <div>Hi there</div>
-      </main>
-      <section className='last:mt-auto'>
-        <Footer />
-      </section>
-    </div>
+      <Landing />
   );
 }
