c2c6602654dde1e540ffbd84945ada0b68c675da
nicolastoon <nicolastoon05@gmail.com>
2026-02-07 21:32:12 -0800

    setting up github pages workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  reactStrictMode: true,
+  basePath: "/sen-conference", // change this when launching to custom domain
+  images: {
+    unoptimized: true,
+  }
 };
 
 export default nextConfig;
