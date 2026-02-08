5f62a69c2594afeb8c79f400cda7682e36f6b478
nicolastoon <nicolastoon05@gmail.com>
2026-02-08 01:35:56 -0800

    new header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,19 +1,27 @@
-import Image from 'next/image'
-
 export default function Header() {
   return (
     <section>
-        <div className='flex w-full h-[160px] p-16 bg-[#0D1626]'>
-            <img src="/sen-conference/assets/SEN_full_logo_light_text.png" alt='SEN Logo' className='h-full w-auto' />
-            <div className='flex w-[858px] h-[160px] justify-between items-center font-bold text-white mr-[50px]'>
-                <div>HOME</div>
-                <div>SCHEDULE</div>
-                <div>SPEAKERS</div>
-                <div>PARTNERS</div>
-                <div>FAQ</div>
-                <div>REGISTER</div>
-            </div>
+      <div className="flex flex-row w-full h-[10vh] p-4 gap-4 bg-sen-blue items-center">
+        <img
+          src="/sen-conference/assets/SEN_small_logo.png"
+          alt="SEN Badge"
+          className="h-full w-auto"
+        />
+        <div className="flex text-white gap-2 items-end">
+          <span className="font-medium text-4xl">Blueprint Summit 2026</span>
+          <span className="font-normal">
+            SEN’s Startup & Innovation Conference
+          </span>
         </div>
+        <div className="flex flex-row ml-auto gap-4 bg-white px-8 py-3 rounded-full font-medium text-center text-lg">
+          <span>La Jolla, CA</span>
+          <span>—</span>
+          <span>May 1, 2026</span>
+        </div>
+        <div className="bg-sen-yorange px-8 py-3 rounded-full text-xl font-semibold text-center">
+          Get Tickets ↗
+        </div>
+      </div>
     </section>
-  )
+  );
 }
