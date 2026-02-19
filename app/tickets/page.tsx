1aaababaae35a4d952b390909aa747b0a4ef13a4
nicolastoon <nicolastoon05@gmail.com>
2026-02-19 00:51:43 -0800

    setup stripe checkout

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,26 +1,28 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import Form from "next/form";
 import TicketsHeader from "./components/TicketsHeader";
+import handleCheckout from "../scripts/handleCheckout";
 
 export default function TicketsPage() {
   const router = useRouter();
 
   return (
     <main className="flex justify-center py-[5vh] px-4 bg-linear-to-r from-sen-blue from-50% to-[#5F6B80]">
-      <div className="h-[90vh] w-full max-w-3xl bg-white rounded-xl shadow-md p-10">
+      <div className="h-[90vh] w-full max-w-3xl bg-white rounded-xl shadow-[0px_0px_20px_white] p-10">
         <TicketsHeader />
-        
+
         <div className="flex justify-center mb-12">
           <div className="w-[420px]">
             <div className="relative flex items-center w-full">
-              <div className="absolute w-full h-[2px] bg-[#0D1626]" />
+              <div className="absolute w-full h-[2px] bg-sen-yorange" />
 
-              <div className="relative z-10 w-5 h-5 rounded-full bg-[#0D1626] border-3 border-sen-yorange p-2.5" />
+              <div className="relative z-10 w-5 h-5 rounded-full bg-sen-yorange border-3 border-sen-yorange p-2.5" />
 
               <div className="flex-1" />
 
-              <div className="relative z-10 w-5 h-5 rounded-full bg-[#0D1626]" />
+              <div className="relative z-10 w-5 h-5 rounded-full bg-sen-yorange" />
             </div>
 
             <div className="flex justify-between mt-3 text-base">
@@ -29,80 +31,90 @@ export default function TicketsPage() {
             </div>
           </div>
         </div>
-        <div className='flex flex-col h-[60vh]'>
-        <div className="grid grid-cols-2 gap-6">
-          <div className="flex flex-col">
-            <label className="text-sm font-medium mb-1">
-              Attendee First Name *
-            </label>
-            <input
-              className="bg-[#D9D9D9] rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-[#F6B654]"
-              type="text"
-            />
+
+        <Form action={handleCheckout} className="flex flex-col h-[60vh]">
+          <div className="grid grid-cols-2 gap-6">
+            <div className="flex flex-col">
+              <label className="text-sm font-medium mb-1">
+                Attendee First Name *
+              </label>
+              <input
+                className="bg-[#D9D9D9] rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-sen-yorange"
+                name='first-name'
+                type="text"
+              />
+            </div>
+
+            <div className="flex flex-col">
+              <label className="text-sm font-medium mb-1">
+                Attendee Last Name *
+              </label>
+              <input
+                className="bg-[#D9D9D9] rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-sen-yorange"
+                name='last-name'
+                type="text"
+              />
+            </div>
+
+            <div className="col-span-2 flex flex-col">
+              <label className="text-sm font-medium mb-1">
+                Attendee Email *
+              </label>
+              <input
+                className="bg-[#D9D9D9] rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-sen-yorange"
+                name='email'
+                type="email"
+              />
+            </div>
+
+            <div className="flex flex-col">
+              <label className="text-sm font-medium mb-1">City *</label>
+              <input
+                className="bg-[#D9D9D9] rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-sen-yorange"
+                name='city'
+                type="text"
+              />
+            </div>
+
+            <div className="flex flex-col">
+              <label className="text-sm font-medium mb-1">State *</label>
+              <input
+                className="bg-[#D9D9D9] rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-sen-yorange"
+                name='state'
+                type="text"
+              />
+            </div>
+
+            <div className="col-span-2 flex flex-col">
+              <label className="text-sm font-medium mb-1">
+                Choose Ticket *
+              </label>
+              <select name="ticket" className="bg-[#D9D9D9] rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-sen-yorange">
+                <option>General Admission</option>
+                <option>Student</option>
+                <option>VIP</option>
+              </select>
+            </div>
+
+            <div className="col-span-2 flex flex-col">
+              <label className="text-sm font-medium mb-1">
+                Have a referral code from a friend?
+              </label>
+              <input
+                className="bg-[#D9D9D9] rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-sen-yorange"
+                name="referral-code"
+                type="text"
+              />
+            </div>
           </div>
 
-          <div className="flex flex-col">
-            <label className="text-sm font-medium mb-1">
-              Attendee Last Name *
-            </label>
-            <input
-              className="bg-[#D9D9D9] rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-[#F6B654]"
-              type="text"
-            />
-          </div>
-
-          <div className="col-span-2 flex flex-col">
-            <label className="text-sm font-medium mb-1">Attendee Email *</label>
-            <input
-              className="bg-[#D9D9D9] rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-[#F6B654]"
-              type="email"
-            />
-          </div>
-
-          <div className="flex flex-col">
-            <label className="text-sm font-medium mb-1">City *</label>
-            <input
-              className="bg-[#D9D9D9] rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-[#F6B654]"
-              type="text"
-            />
-          </div>
-
-          <div className="flex flex-col">
-            <label className="text-sm font-medium mb-1">State *</label>
-            <input
-              className="bg-[#D9D9D9] rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-[#F6B654]"
-              type="text"
-            />
-          </div>
-
-          <div className="col-span-2 flex flex-col">
-            <label className="text-sm font-medium mb-1">Choose Ticket *</label>
-            <select className="bg-[#D9D9D9] rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-[#F6B654]">
-              <option>General Admission</option>
-              <option>Student</option>
-              <option>VIP</option>
-            </select>
-          </div>
-
-          <div className="col-span-2 flex flex-col">
-            <label className="text-sm font-medium mb-1">
-              Have a referral code from a friend?
-            </label>
-            <input
-              className="bg-[#D9D9D9] rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-[#F6B654]"
-              type="text"
-            />
-          </div>
-        </div>
-
-        {/* TODO: stop people from clicking 'check out' button when not all required fields are filled out */}
-        <button
-          onClick={() => router.push("/tickets/payment")}
-          className="mt-auto w-full bg-[#0D1626] text-white py-3 rounded-lg text-lg font-semibold hover:opacity-90 transition"
-        >
-          Check Out
-        </button>
-        </div>
+          <button
+            type='submit'
+            className="mt-auto w-full bg-sen-blue text-white py-3 rounded-lg text-lg font-semibold hover:opacity-90 transition"
+          >
+            Check Out
+          </button>
+        </Form>
       </div>
     </main>
   );
